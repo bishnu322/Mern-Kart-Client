34a7b9fbefa4d8a42ae2dd6a1ffc9f925820a20f
bishnu_kohar <koharbishnu322@gmail.com>
2025-09-04 16:20:46 +0545

    creating admin layout and creating sidenav

---
+++ src/components/admin/sidebar/SidebarLinks.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { MdOutlineDashboard } from "react-icons/md";
+import { Link, useLocation } from "react-router";
+import { PiUsersThreeBold } from "react-icons/pi";
+import { RiListCheck3 } from "react-icons/ri";
+import { MdOutlineShoppingBag } from "react-icons/md";
+import { GiStamper } from "react-icons/gi";
+import { IoPricetagsOutline } from "react-icons/io5";
+
+const SidebarLinks = () => {
+  const location = useLocation();
+
+  const isActive = location.pathname;
+  const links = [
+    {
+      label: "Dashboard",
+      link: "/admin",
+      icon: <MdOutlineDashboard />,
+    },
+    {
+      label: "Brands",
+      link: "/admin",
+      icon: <GiStamper />,
+    },
+    {
+      label: "Category",
+      link: "/admin",
+      icon: <IoPricetagsOutline />,
+    },
+    {
+      label: "Products",
+      link: "/admin",
+      icon: <MdOutlineShoppingBag />,
+    },
+    {
+      label: "Orders",
+      link: "/admin/orders",
+      icon: <RiListCheck3 />,
+    },
+    {
+      label: "Users",
+      link: "/admin/user",
+      icon: <PiUsersThreeBold />,
+    },
+  ];
+  return (
+    <div>
+      {links.map((items) => (
+        <Link
+          to={items.link}
+          key={items.link}
+          className={`flex items-center p-2 gap-2 mx-3 font-semibold text-lg rounded hover:bg-violet-500 hover:text-white transition-all duration-200 ${
+            isActive === items.link ? "bg-violet-500 text-white" : ""
+          }`}
+        >
+          <div>{items.icon}</div>
+          <div>{items.label}</div>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default SidebarLinks;
