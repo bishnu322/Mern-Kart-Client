a2fd9300961b8d0853cc6a0985867ee55f2379d1
bishnu_kohar <koharbishnu322@gmail.com>
2025-09-11 14:00:07 +0545

    product and brand initial view page

--- src/pages/admin/brand/Brand.tsx
+++ src/pages/admin/brand/Brand.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import PageHeader from "../../../components/admin/header/PageHeader";
+import AdminBodyWrapper from "../../../components/admin/form/AdminBodyWrapper";
+import BrandList from "../../../components/admin/brand/BrandList";
 
 const Brand = () => {
-  return <div>Brand</div>;
+  return (
+    <main>
+      <PageHeader
+        title="Brand list"
+        subTitle="All product brand list"
+        buttonText="Add Brand"
+        linkTo="/admin/brand/add"
+      />
+
+      <AdminBodyWrapper>
+        <BrandList />
+      </AdminBodyWrapper>
+    </main>
+  );
 };
 
 export default Brand;
--- src/pages/admin/product/Product.tsx
+++ src/pages/admin/product/Product.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import PageHeader from "../../../components/admin/header/PageHeader";
 
 const Products = () => {
-  return <div>Product</div>;
+  return (
+    <main>
+      <PageHeader
+        title="Product list"
+        subTitle="All products fetched"
+        linkTo="/admin/product/add"
+        buttonText="Add product"
+      />
+    </main>
+  );
 };
 
 export default Products;
