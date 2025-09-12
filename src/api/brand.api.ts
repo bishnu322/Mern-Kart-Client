4fa292cf5a04c7b107ef93244c9c6ce2f1e31091
bishnu_kohar <koharbishnu322@gmail.com>
2025-09-12 20:11:31 +0545

    fetching brand

--- src/api/brand.api.ts
+++ src/api/brand.api.ts
@@ -2,11 +2,23 @@ import { type IBrand } from "./../types/brand.types";
 import { type TResponse } from "./../types/generic";
 import api from "./index";
 
-//* get all the brand
+//* fetching all the brand
 
-export const getAllBrand = async (): TResponse<IBrand[]> => {
-  const response = await api.get<TResponse<IBrand[]>>("/brand");
+export const getAllBrand = async (params: {
+  query?: string;
+}): TResponse<IBrand[]> => {
+  const response = await api.get<TResponse<IBrand[]>>(`/brand`, { params });
   console.log(response.data);
+
+  return response.data;
+};
+
+//* fetching brand by id
+
+export const getBrandById = async (query: string): TResponse<IBrand> => {
+  const response = await api.get<TResponse<IBrand>>(`/brand/${query}`);
+
+  console.log(response);
 
   return response.data;
 };
