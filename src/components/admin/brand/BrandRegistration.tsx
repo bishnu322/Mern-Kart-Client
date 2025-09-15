ec25e64afcf6d84da963c087b9d1a948e4880b88
bishnu_kohar <koharbishnu322@gmail.com>
2025-09-15 18:42:53 +0545

    adding admin title

--- src/components/admin/brand/BrandRegistration.tsx
+++ src/components/admin/brand/BrandRegistration.tsx
@@ -5,6 +5,7 @@ import { useForm } from "react-hook-form";
 import { useMutation } from "@tanstack/react-query";
 import { createBrand } from "../../../api/brand.api";
 import toast from "react-hot-toast";
+import AdminBodyTitle from "../../../shared/designSystem/AdminBodyTitle";
 
 export interface IBrandType {
   brand_name: string;
@@ -38,53 +39,56 @@ const BrandForm = () => {
   };
 
   return (
-    <form
-      className="w-full h-screen flex flex-col gap-2"
-      onSubmit={handleSubmit(handleFormSubmit)}
-    >
-      <div className="grid  gap-2 sm:grid-cols-2">
-        {/* brand name field */}
-        <div>
-          <Input
-            label="Name"
-            id="brand_name"
-            placeholder="Brand name"
-            className="w-full border border-violet-600 p-2 rounded outline-none"
-            {...register("brand_name")}
-            error={errors.brand_name ? errors.brand_name.message : ""}
+    <>
+      <AdminBodyTitle>Add Brand Form</AdminBodyTitle>
+      <form
+        className="w-full h-screen flex flex-col gap-2"
+        onSubmit={handleSubmit(handleFormSubmit)}
+      >
+        <div className="grid  gap-2 sm:grid-cols-2">
+          {/* brand name field */}
+          <div>
+            <Input
+              label="Name"
+              id="brand_name"
+              placeholder="Brand name"
+              className="w-full border border-violet-600 p-2 rounded outline-none"
+              {...register("brand_name")}
+              error={errors.brand_name ? errors.brand_name.message : ""}
+            />
+          </div>
+
+          {/* brand logo field */}
+          <div>
+            <Input
+              label="Brand logo"
+              id="logo"
+              placeholder="Brand image"
+              type="file"
+              className="w-full border border-dashed border-violet-600 p-2 rounded outline-none"
+              {...register("logo")}
+              error={errors.logo ? errors.logo.message : ""}
+            />
+          </div>
+        </div>
+
+        {/* brand description field */}
+        <div className="w-full grid">
+          <TextArea
+            label="Brand description"
+            id="description"
+            placeholder="Add brand description"
+            className="min-h-[200px] border border-violet-600 p-2 rounded outline-none"
+            {...register("description")}
+            error={errors.description ? errors.description.message : ""}
           />
         </div>
 
-        {/* brand logo field */}
-        <div>
-          <Input
-            label="Brand logo"
-            id="logo"
-            placeholder="Brand image"
-            type="file"
-            className="w-full border border-dashed border-violet-600 p-2 rounded outline-none"
-            {...register("logo")}
-            error={errors.logo ? errors.logo.message : ""}
-          />
+        <div className="mt-3 w-1/4">
+          <Button type="submit">{isPending ? "Submitting" : "Submit"}</Button>
         </div>
-      </div>
-
-      {/* brand description field */}
-      <div className="w-full grid">
-        <TextArea
-          label="Brand description"
-          id="description"
-          placeholder="Add brand description"
-          className="min-h-[200px] border border-violet-600 p-2 rounded outline-none"
-          {...register("description")}
-          error={errors.description ? errors.description.message : ""}
-        />
-      </div>
-
-      <div className="mt-3 w-1/4">
-        <Button type="submit">{isPending ? "Submitting" : "Submit"}</Button>
-      </div>
-    </form>
+      </form>
+    </>
   );
 };
 
--- src/components/admin/brand/BrandUpdate.tsx
+++ src/components/admin/brand/BrandUpdate.tsx
@@ -7,6 +7,7 @@ import { getBrandById, UpdateBrand } from "../../../api/brand.api";
 // import toast from "react-hot-toast";
 import { useParams } from "react-router";
 import toast from "react-hot-toast";
+import AdminBodyTitle from "../../../shared/designSystem/AdminBodyTitle";
 
 export interface IBrandType {
   brand_name: string;
@@ -56,53 +57,56 @@ const BrandUpdate = () => {
   };
 
   return (
-    <form
-      className="w-full h-screen flex flex-col gap-2"
-      onSubmit={handleSubmit(onSubmit)}
-    >
-      <div className="grid  gap-2 sm:grid-cols-2">
-        {/* brand name field */}
-        <div>
-          <Input
-            label="Name"
-            id="brand_name"
-            placeholder="Brand name"
-            className="w-full border border-violet-600 p-2 rounded outline-none"
-            {...register("brand_name")}
-            error={errors.brand_name ? errors.brand_name.message : ""}
+    <>
+      <AdminBodyTitle>Brand Update Field</AdminBodyTitle>
+      <form
+        className="w-full h-screen flex flex-col gap-2"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <div className="grid  gap-2 sm:grid-cols-2">
+          {/* brand name field */}
+          <div>
+            <Input
+              label="Name"
+              id="brand_name"
+              placeholder="Brand name"
+              className="w-full border border-violet-600 p-2 rounded outline-none"
+              {...register("brand_name")}
+              error={errors.brand_name ? errors.brand_name.message : ""}
+            />
+          </div>
+
+          {/* brand logo field */}
+          <div>
+            <Input
+              label="Brand logo"
+              id="logo"
+              placeholder="Brand image"
+              type="file"
+              className="w-full border border-dashed border-violet-600 p-2 rounded outline-none"
+              {...register("logo")}
+              error={errors.logo ? errors.logo.message : ""}
+            />
+          </div>
+        </div>
+
+        {/* brand description field */}
+        <div className="w-full grid">
+          <TextArea
+            label="Brand description"
+            id="description"
+            placeholder="Add brand description"
+            className="min-h-[200px] border border-violet-600 p-2 rounded outline-none"
+            {...register("description")}
+            error={errors.description ? errors.description.message : ""}
           />
         </div>
 
-        {/* brand logo field */}
-        <div>
-          <Input
-            label="Brand logo"
-            id="logo"
-            placeholder="Brand image"
-            type="file"
-            className="w-full border border-dashed border-violet-600 p-2 rounded outline-none"
-            {...register("logo")}
-            error={errors.logo ? errors.logo.message : ""}
-          />
+        <div className="mt-3 w-1/4">
+          <Button type="submit">{isPending ? "Updating" : "Update"}</Button>
         </div>
-      </div>
-
-      {/* brand description field */}
-      <div className="w-full grid">
-        <TextArea
-          label="Brand description"
-          id="description"
-          placeholder="Add brand description"
-          className="min-h-[200px] border border-violet-600 p-2 rounded outline-none"
-          {...register("description")}
-          error={errors.description ? errors.description.message : ""}
-        />
-      </div>
-
-      <div className="mt-3 w-1/4">
-        <Button type="submit">{isPending ? "Updating" : "Update"}</Button>
-      </div>
-    </form>
+      </form>
+    </>
   );
 };
 
