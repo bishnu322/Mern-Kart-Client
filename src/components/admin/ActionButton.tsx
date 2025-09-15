3cad13dffd9a0579695912112e7e26116ed2525a
bishnu_kohar <koharbishnu322@gmail.com>
2025-09-15 23:19:57 +0545

    edit and delete action button component

---
+++ src/components/admin/ActionButton.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { FaEdit, FaRegTrashAlt } from "react-icons/fa";
+import { Link } from "react-router";
+
+interface IProps {
+  LinkTo: string;
+  onClick: () => void;
+}
+
+const ActionButton: React.FC<IProps> = ({ LinkTo, onClick }) => {
+  console.log(LinkTo);
+  return (
+    <div className="flex justify-center gap-4">
+      <Link
+        to={`/admin/category/${LinkTo}`}
+        className="text-orange-500 cursor-pointer"
+      >
+        <FaEdit size={20} />
+      </Link>
+      <span className="text-red-600 cursor-pointer">
+        <FaRegTrashAlt size={20} onClick={onClick} />
+      </span>
+    </div>
+  );
+};
+
+export default ActionButton;
