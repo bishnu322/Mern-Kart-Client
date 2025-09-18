3325dd584938f0a2e4034722bb92c7b741cb4839
bishnu_kohar <koharbishnu322@gmail.com>
2025-09-18 15:53:25 +0545

    page for update product

--- src/pages/admin/product/UpdateProduct.tsx
+++ src/pages/admin/product/UpdateProduct.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { useParams } from "react-router";
+import PageHeader from "../../../components/admin/header/PageHeader";
 
 const UpdateProduct = () => {
   const params = useParams();
 
   console.log({ params });
-  return <div>UpdateProduct</div>;
+  return (
+    <main>
+      <PageHeader
+        title="Update product"
+        subTitle="All field to update"
+        buttonText="Product list"
+        linkTo="/admin/product"
+      />
+    </main>
+  );
 };
 
 export default UpdateProduct;
