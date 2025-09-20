cae0f5a6f17230585cc45958270c5338cbc26ad9
bishnu_kohar <koharbishnu322@gmail.com>
2025-09-20 23:58:55 +0545

    update api and remove product api

--- src/api/product.api.ts
+++ src/api/product.api.ts
@@ -9,7 +9,7 @@ export const getAllProduct = async (): TGetAllProduct => {
   return response.data;
 };
 
-//* get all product
+//* get all featured product
 // type TGetAllProduct = TResponse<IProduct[]>;
 export const getAllFeaturedProduct = async (): TGetAllProduct => {
   const response = await api.get<TGetAllProduct>("/product/featuredProduct");
@@ -67,4 +67,54 @@ export const createProduct = async (data: {
   return response.data;
 };
 
+//* update product
+export const updateProduct = async (
+  id: string,
+  data: {
+    name: string;
+    description: string;
+    price: number;
+    stock: number;
+    category: string;
+    brand: string;
+    isFeatured: boolean;
+    cover_img: FileList;
+    images: FileList;
+  }
+) => {
+  const formData = new FormData();
+
+  formData.append("name", data.name);
+  formData.append("description", data.description);
+  formData.append("price", data.price.toString());
+  formData.append("stock", data.stock.toString());
+  formData.append("category", data.category);
+  formData.append("brand", data.brand);
+  formData.append("isFeatured", data.isFeatured.toString());
+
+  if (data.cover_img && data.cover_img.length > 0) {
+    formData.append("cover_img", data.cover_img[0]);
+  }
+
+  if (data.images && data.images.length > 0) {
+    Array.from(data.images).forEach((file) => {
+      formData.append("images", file);
+    });
+  }
+
+  const response = await api.put(`/product/${id}`, formData);
+
+  console.log(response.data);
+
+  return response.data;
+};
+
 // * remove product
+
+export const removeProduct = async (id: string) => {
+  const response = await api.delete(`/product/${id}`);
+
+  console.log(response.data);
+
+  return response.data;
+};
