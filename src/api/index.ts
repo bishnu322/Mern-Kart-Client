f741e0eb6b183c0a649c419b0b9d3864675158ce
bishnu_kohar <koharbishnu322@gmail.com>
2025-12-21 00:14:32 +0545

    reverted env

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
+  baseURL: "https://mern-kart-hlox.onrender.com/api",
   withCredentials: true,
 });
 
